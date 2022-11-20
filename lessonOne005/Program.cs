81bd7a62be857439c10526f78da9b83c257446fa
Kaspits <azdanova97@gmail.com>
2022-11-20 23:19:20 +0100

    6th changes with favorit username

---
+++ lessonOne005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Add your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha") 
+{
+    Console.WriteLine("Yes, its MASHA!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
