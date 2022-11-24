6148764a1057f50a8edf4b85472d4da165ab9352
Kaspits <azdanova97@gmail.com>
2022-11-24 22:32:04 +0100

    New day

---
+++ lessonTwo004/Program.cs
@@ -0,0 +1,49 @@
+﻿void FillArray(int[] collaction)
+{
+    int length = collaction.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collaction[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collaction, int find)
+{
+    int count = collaction.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collaction[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+Console.WriteLine(pos);
